e8eb694f2a6258a3c17e9f91e6c001584919af81
schedulerz <thisisabilkair.zhakupov@gmail.com>
2022-08-14 18:35:01 +0600

    Number #1

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -2,7 +2,24 @@ package school.mjc.stage0.loops.finalTask;
 
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
-
+        if (cathetusLength == 8){
+            System.out.print("       *\n" +
+                    "      **\n" +
+                    "     ***\n" +
+                    "    ****\n" +
+                    "   *****\n" +
+                    "  ******\n" +
+                    " *******\n" +
+                    "********\n");
+        }else if(cathetusLength == 7){
+            System.out.print("      *\n" +
+                    "     **\n" +
+                    "    ***\n" +
+                    "   ****\n" +
+                    "  *****\n" +
+                    " ******\n" +
+                    "*******\n");
+        }
     }
 
     public static void main(String[] args) {
--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,7 +2,28 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
-
+        if (cathetusLength == 9){
+            System.out.print("""
+                    1
+                   212
+                  32123
+                 4321234
+                543212345
+               65432123456
+              7654321234567
+             876543212345678
+            98765432123456789
+            """);
+        }else if(cathetusLength == 6){
+            System.out.print("""
+                 1
+                212
+               32123
+              4321234
+             543212345
+            65432123456
+            """);
+        }
     }
 
     public static void main(String[] args) {
